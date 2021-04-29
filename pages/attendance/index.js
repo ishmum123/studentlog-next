6f0c7456ea396fad09367599a2cec18cf2eab93e
lubna40 <lubu7035@gmail.com>
2021-04-29 15:21:14 +0600

    Fetch data using axois from json server

--- pages/attendance/index.js
+++ pages/attendance/index.js
@@ -1,8 +1,23 @@
-import { dummyAttendanceList } from "../../dummy.data";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import AttendanceList from "./AttendanceList";
 
+const BASE_URL = "http://localhost:3001/attendances"
+
 const AttendanceHome = () => {
-  const attendanceList = dummyAttendanceList
+  const [attendanceList, setAttendanceList] = useState("");
+  
+  useEffect( async () => {
+    await axios.get(BASE_URL)
+    .then(res => {
+      console.log(res.data)
+      setAttendanceList(res.data);
+    })
+    .catch(error => {
+      console.log("Error occured " + error);
+    })
+  }, [setAttendanceList])
+
   return (
     <AttendanceList attendanceList={ attendanceList }/>
   );
