8e9633d03223e51fc7aa9cfd8ff334a7cc9583c0
Ishmum Jawad Khan <ishmumkhan@gmail.com>
2021-05-17 16:29:37 +0600

    [feature] dashboard translations

--- modules/shared/dashboard.js
+++ modules/shared/dashboard.js
@@ -7,18 +7,18 @@ import styles from './dashboard.module.css';
 
 const Dashboard = ({children, t, i18n}) => {
   const items = [
-    {label: 'Student List', url: '/attendance/'},
-    {label: 'Attendance List', url: '/attendance/list'},
-    {label: 'New Leave Application', url: '/leave-application/new-application'},
-    {label: 'Pending Leave Applications', url: '/leave-application/pending-applications'},
-    {label: 'Student Registration', url: '/registration'}
+    {label: t('students'), url: '/attendance/'},
+    {label: t('attendance'), url: '/attendance/list'},
+    {label: t('new_leave_application'), url: '/leave-application/new-application'},
+    {label: t('pending_leave_applications'), url: '/leave-application/pending-applications'},
+    {label: t('student_registration'), url: '/registration'}
   ]
 
   return (
     <>
       <Menu id="navbar" model={items}/>
       <div className={styles.content}>
-        <header className={styles.topright}>
+        <header className={styles.header}>
           <nav>
             <Button
               onClick={() => window.location.href = keycloak.keycloak.createLogoutUrl()}
@@ -35,7 +35,7 @@ const Dashboard = ({children, t, i18n}) => {
             />
           </nav>
         </header>
-        {children}
+        <div className={styles.children}>{children}</div>
         <SelectButton
           value={i18n.language}
           options={config.allLanguages}
--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ const NextI18Next = require("next-i18next").default;
 const path = require("path");
 
 module.exports = new NextI18Next({
-  defaultLanguage: "en",
-  otherLanguages: ["bn"],
+  defaultLanguage: "bn",
+  otherLanguages: ["en"],
   localePath: path.resolve("./public/locales"),
 });
